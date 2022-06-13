7cb89e1c512ccb7556d5e6dccf7aec8db53c8cd7
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-06-13 22:40:50 +0100

    add Photographer Contructor

---
+++ scripts/Models/Photographer.js
@@ -0,0 +1,34 @@
+class Photographer {
+    constructor(data) {
+        this._name = data.name
+        this._city = data.city
+        this._country = data.country
+        this._tagline = data.tagline
+        this._price = data.price
+        this._portrait = data.portrait
+    }
+
+    get getName() {
+        return this._name
+    }
+
+    get getCity() {
+        return this._city
+    }
+
+    get getCountry() {
+        return this._country
+    }
+
+    get getTagline() {
+        return this._tagline
+    }
+
+    get getPrice() {
+        return this._price
+    }
+
+    get getPortrait() {
+        return this._portrait
+    }
+}
