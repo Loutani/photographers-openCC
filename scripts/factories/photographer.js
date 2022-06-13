9b0a158f62bb8a936a795402c1a9cb65a41b0281
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-06-13 23:09:53 +0100

    complete the photographer card design

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, id, price, tagline } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +9,21 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const div = document.createElement('div');
+        const h4 = document.createElement('h4');
+        h4.textContent = city + ', ' + country;
+        const p = document.createElement('p');
+        p.textContent = tagline;
+        const pPrice = document.createElement('p');
+        pPrice.textContent = price + '€/jour';
+        div.appendChild(h4);
+        div.appendChild(p);
+        div.appendChild(pPrice);
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(div);
         return (article);
     }
+
     return { name, picture, getUserCardDOM }
 }
