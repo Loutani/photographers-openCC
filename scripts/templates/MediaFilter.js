5164499ce4f6984972020c87648c1bea3e357f66
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-06-14 02:23:27 +0100

    revert media directories name and replace the directory name search function

--- scripts/templates/MediaFilter.js
+++ scripts/templates/MediaFilter.js
@@ -3,23 +3,42 @@ class MediaFilter {
         this._medias        = medias
         this._photographer  = photographer
         this.$wrapper       = document.querySelector('.media-filter-container')
+        this.directoriesName= [
+            'Ellie Rose',
+            'Marcel',
+            'Mimi',
+            'Nabeel',
+            'Rhode',
+            'Tracy',
+        ]
     }
 
     createMedias() {
         let mediaContent = ``;
-
-        this._medias.map(media => {
+        const photographerName = this.directoriesName.find(name => this._photographer._name.includes(name))
+        
+        this._medias.
+        map(media => {
             let mediaData = new Media(media)
-            mediaContent += new MediaFactory(mediaData, this._photographer._name).render()
+            mediaContent += new MediaFactory(mediaData, photographerName).render()
         });
 
         return mediaContent
     }
 
+    createFilter() {
+        return `
+            <div class="">
+
+            </div>
+        `
+    }
+
     createMediaFilter() {
         return `
             <div class="filter">
-                <p>Trier par<p>
+                <p>Trier par</p>
+                ${this.createFilter()}
             </div>
             <div class="media-content">
                 ${this.createMedias()}
