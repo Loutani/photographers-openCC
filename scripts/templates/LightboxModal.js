36ce7f0d5209d7247da44873b2bfc0eec42414ca
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-06-22 22:16:50 +0100

    use exist function in close lightbox by press escape

--- scripts/templates/LightboxModal.js
+++ scripts/templates/LightboxModal.js
@@ -67,24 +67,9 @@ class LightboxModal {
 
     //hide light box by press escape keyboard
     closeLightBoxByKeyboard() {
-        document.querySelector('.media-content').addEventListener('keydown', function(e) {
-
-            if(e.key == "Escape") {
-                //return scroll to the body HTML element
-                document.body.classList.remove('has-open-lightbox')
-                
-                //hide lightbox and empty it's HTML content
-                this._selectedCard = {
-                    opened      : false,
-                    htmlContent : ``,
-                    selectedId  : null
-                }
-
-                //empty the light box HTML content
-                document.querySelector('.lightbox-modal-body').innerHTML = this._selectedCard.htmlContent
-
-                //hide the light box
-                document.querySelector('.lightbox-modal').classList.remove('show')
+        document.querySelector('.media-content').addEventListener('keydown', event => {
+            if(event.key == "Escape") {
+                this.hideLightBox()
             }
         });
     }
