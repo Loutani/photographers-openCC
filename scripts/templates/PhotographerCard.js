fe6882c4a35150f61b6a49e81fac3b3a619d79aa
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-06-22 20:13:35 +0100

    add tabindex to photgrapher card container

--- scripts/templates/PhotographerCard.js
+++ scripts/templates/PhotographerCard.js
@@ -1,25 +1,34 @@
 class PhotographerCard {
+    
+    //init the photographer card
     constructor(photographer) {
+        //photographer data
         this._photographer = photographer
+
+        //photographer card container
         this.$wrapper = document.createElement('article')
     }
 
+    //add click event listner to photgrapher image
     onCardImageClick() {
         this.$wrapper.querySelector('img').addEventListener('click', () => {
             location.href = 'photographer.html?id=' + this._photographer._id
         });
     }
 
+    //add click event listener to photographer name
     onCardNameClick() {
         this.$wrapper.querySelector('h2').addEventListener('click', () => {
             location.href = 'photographer.html?id=' + this._photographer._id
         });
     }
 
-    createPhotographerCard() {
+    //create Photographer Card container
+    createPhotographerCard(index) {
+        //create photographer card html content
         const photographerCard = `
                                     <img alt="${this._photographer._name} image" title="${this._photographer._name}" src="assets/photographers/${this._photographer._portrait}">
-                                    <h2 title="${this._photographer._name}">${this._photographer._name}</h2>
+                                    <h2 tabindex="${index}" title="${this._photographer._name}">${this._photographer._name}</h2>
                                     <div class="photographer_paragraph">
                                         <h4>${this._photographer._city}, ${this._photographer._country}</h4>
                                         <p>${this._photographer._tagline}</p>
@@ -27,11 +36,16 @@ class PhotographerCard {
                                     </div>
                                 `
 
+        //append the photographer card html content to container
         this.$wrapper.innerHTML = photographerCard
 
+        //add click event on photographer image
         this.onCardImageClick();
+
+        //add click event on photographer name
         this.onCardNameClick();
 
+        //return the container
         return this.$wrapper
     }
 }
