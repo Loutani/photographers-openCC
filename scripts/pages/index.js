95f15f9e9a92af418388ece3c3d7c13d443cd7c4
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-06-22 20:14:16 +0100

    add index for photographer card creator to use it as tabindex

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,21 +1,38 @@
 class Index {
+
+    //init home page
     constructor() {
+
+        //photographers container
         this.$photographerWrapper   = document.querySelector(".photographer_section");
+
+        //photgraphers API
         this.photographerApi        = new PhotographerApi('data/photographers.json')
     }
 
+    //get the photographers data and create it's card
     async main() {
+
+        //get photographsers data
         const photographerData = await this.photographerApi.getPhotographers();
 
+        //create photographers data using constructor design pattern
         const photographers = await photographerData.photographers.map(photographer => new Photographer(photographer));
 
-        photographers.map(photographer => {
-            const photographerCard = new PhotographerCard(photographer).createPhotographerCard()
+        //loop for every photographer to create it's card
+        photographers.map((photographer, index) => {
+            
+            //create photographer card
+            const photographerCard = new PhotographerCard(photographer).createPhotographerCard(index + 1)
+
+            //append the photographer card to photographers container
             this.$photographerWrapper.appendChild(photographerCard)
         })
     }
 }
 
+//create new home page
 const index = new Index()
 
+//create home page content
 index.main();
