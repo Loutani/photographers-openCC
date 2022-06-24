0ad2756b40da91aef5cf2a84f56be30830fbfb00
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-06-24 15:11:13 +0100

    contact page show data and reset the input and text area

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -9,3 +9,33 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+function submitForm(event) {
+    //stop reload page by the submit effect
+    event.preventDefault()
+
+    let inputs = document.querySelectorAll('input'),
+        message = document.querySelector('textarea'),
+        data = {};
+
+    //get all the data from inputs
+    inputs.forEach(input => {
+        //get the data
+        data[input.id] = input.value;
+
+        //reset the data
+        input.value = ""
+    })
+
+    //get the message from textarea
+    data[message.id] = message.value
+
+    //reset the data
+    message.value = "";
+
+    //show datain console
+    console.log(data)
+
+    //hide the modal
+    closeModal()
+}
